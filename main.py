bfe7738d9877d4c9c824ffb5f1a77dfb5ed89bcd
Iulian Peiu <iulianpeiu6@gmail.com>
2021-10-06 01:28:32 +0300

    Lab2 - Implement move_person & is_valid

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 class Constants:
     @staticmethod
-    def left_side_land():
-        return "left_land"
+    def left_bank():
+        return "left_bank"
 
     @staticmethod
-    def right_side_land():
-        return "right_land"
+    def right_bank():
+        return "right_bank"
 
     @staticmethod
     def female_genre():
@@ -17,27 +17,58 @@ class Constants:
 
 
 class SolutionStage:
+
     def __init__(self, n):
         self.couples = []
-        for index in range(n):
-            self.couples.insert(index, Couple(index, Constants.left_side_land()))
+        self.all_men = []
+        self.all_women = []
 
-    def print_current_solution(self):
+        for index in range(n):
+            current_couple = Couple(index, Constants.left_bank())
+            self.couples.insert(index, current_couple)
+            self.all_men.insert(index, current_couple.man)
+            self.all_women.insert(index, current_couple.woman)
+
+    def show(self):
         for couple in self.couples:
             print(f"{ couple.id }: [{ couple.man } { couple.woman }]")
 
-    def solution_is_final(self):
+    def is_final(self):
         for couple in self.couples:
-            if couple.man.location == Constants.left_side_land():
+            if couple.man.location == Constants.left_bank():
                 return False
-            if couple.woman.location == Constants.left_side_land():
+            if couple.woman.location == Constants.left_bank():
                 return False
         return True
 
+    def move_person(self, person, to_bank):
+        if to_bank not in [Constants.left_bank(), Constants.right_bank()]:
+            print("ERR\tPlease provide a valid bank name")
+            return False
+        if person.location == to_bank:
+            print("ERR\tCan\'t move a person from a bank to the same bank")
+            return False
+        person.location = to_bank
+        return True
+
+    def is_valid(self):
+        for couple in self.couples:
+            if couple.man.location == couple.woman.location:
+                continue
+            if self.are_men_on_location(couple.woman.location):
+                return False
+        return True
+
+    def are_men_on_location(self, location):
+        locations = [i.location for i in self.all_men]
+        if location in locations:
+            return True
+        return False
+
 
 class Couple:
-    def __init__(self, id, location):
-        self.id = id
+    def __init__(self, _id, location):
+        self.id = _id
         self.man = Person(Constants.man_genre(), location)
         self.woman = Person(Constants.female_genre(), location)
 
@@ -52,6 +83,10 @@ class Person:
 
 
 if __name__ == '__main__':
-    solution = SolutionStage(4)
-    solution.print_current_solution()
-    print(solution.solution_is_final())
+    solution = SolutionStage(2)
+    solution.show()
+    print(solution.is_valid())
+    solution.move_person(solution.couples[0].man, Constants.right_bank())
+    print(solution.is_valid())
+    solution.show()
+    print(solution.is_final())
