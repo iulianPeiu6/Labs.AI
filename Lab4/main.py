36958b16054e9c86beffc88b24f593070c608a68
Iulian Peiu <iulianpeiu6@gmail.com>
2021-10-24 23:23:38 +0300

    Lab4 - FC - Fix Solution & Refactoring

--- Lab4/main.py
+++ Lab4/main.py
@@ -4,52 +4,44 @@ import sys
 
 class Map(object):
     def __init__(self, _location_neighbours_map, _location_colors_map):
-        self.answer = {}
-        self.neighbours = _location_neighbours_map
-        self.colors = _location_colors_map
+        self.neighbours_mapping = _location_neighbours_map
+        self.colors_mapping = _location_colors_map
         self.visited = set()
-        self.init_answer()
-        
-    def init_answer(self):
-        self.answer = dict(self.colors)
 
     def forward_checking(self, location):
-        for neighbour in self.neighbours[location]:
-            self.colors[neighbour] = self.colors[neighbour] - self.colors[location]
+        for neighbour in self.neighbours_mapping[location]:
+            self.colors_mapping[neighbour] = self.colors_mapping[neighbour] - self.colors_mapping[location]
 
     def is_not_consistent(self):
-        for color in self.colors:
-            print(f"Checking color: {color, len(color)}")
-            if len(color) == 0:
+        for location in self.colors_mapping:
+            if len(self.colors_mapping[location]) == 0:
                 return True
-        print(f"Consisten: {self.colors}")
         return False
 
     def __str__(self) -> str:
-        str = self.colors.__str__()
+        str = self.colors_mapping.__str__()
         return str           
 
 
 def solve_via_fc(map):
-    for location in map.colors:
-        colors = map.colors[location]
+    for location in map.colors_mapping:
+        current_colors = map.colors_mapping[location]
         if location in map.visited:
             continue
         map.visited.add(location)
-        #daca nu avem culorile disponibile
+
         if map.is_not_consistent():
-            #inconsistance
             print(f"DEBUG:\t Inconsistent solution:{map} ")
-            pass
-        if len(map.visited) == len(map.colors.keys()):
+            return
+
+        if len(map.visited) == len(map.colors_mapping.keys()):
             print(f"INFO:\t Solution found: {map}")
             sys.exit()
-        #daca avem culori disponibile
-        for color in colors:
+
+        for color in current_colors:
             next_map = copy.deepcopy(map)
-            next_map.colors[location] = {color}
+            next_map.colors_mapping[location] = {color}
             print(f"DEBUG:\t Checking color {color} for location {location} in map {map}")
-            #forward checking
             next_map.forward_checking(location)
             solve_via_fc(next_map)
         
@@ -76,7 +68,7 @@ if __name__ == '__main__':
         "T": {"red", "blue", "green"}
     }
     problem = Map(location_neighbours_map, location_colors_map)
-    #print(problem.answer)
+
     solve_via_fc(problem)
 
 
