d7d8fdf655c8151088ef3977d9d06e0f99e9dc50
Cezar Lupu <cezarlupu2012@gmail.com>
2021-12-05 23:05:34 +0200

    Lab8-2: superTopicOf - Implementation

--- Lab8/Lab8-2.py
+++ Lab8/Lab8-2.py
@@ -1,8 +1,10 @@
 import lightrdf
 
-doc = lightrdf.RDFDocument("Lab8-1.owl")
-
-for triple in doc.search_triples("http://www.co-ode.org/ontologies/ont.owl#ArtificialIntelligence", None, None):
-    print(triple)
-
-lightrdf.Parser()
+def superTopicOf(topic):
+    BASE_IRI = "http://www.w3.org/2002/07/owl#"
+    doc = lightrdf.RDFDocument("Lab8-1.owl")
+    for triple in doc.search_triples(None, None, None):
+        if "subClassOf" in triple[1] and topic in triple[2]:
+            print(triple[0].split("#")[1])
+            
+superTopicOf("ArtificialIntelligence")
