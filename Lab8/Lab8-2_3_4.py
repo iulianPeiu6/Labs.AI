b147a23c753e198a4c1a9abaaffceda8a672963e
Cezar Lupu <cezarlupu2012@gmail.com>
2021-12-07 00:34:47 +0200

    Lab8-Complete it (3 and 4)

--- Lab8/Lab8-2_3_4.py
+++ Lab8/Lab8-2_3_4.py
@@ -1,9 +1,7 @@
 #Exercise 2
-
 import lightrdf
 
 def super_topic_of(topic):
-    BASE_IRI = "http://www.w3.org/2002/07/owl#"
     doc = lightrdf.RDFDocument("Lab8-1.owl")
     for triple in doc.search_triples(None, None, None):
         if "subClassOf" in triple[1] and topic in triple[2]:
@@ -14,41 +12,61 @@ def super_topic_of(topic):
 
 #Exercise 3
 import nltk
-from nltk.corpus import treebank
 
 def get_nouns_with_verb_between(from_file):
     with open(from_file) as f:
         contents = f.read()
-    tokens = nltk.word_tokenize(contents)
-    #print(tokens)
-    tagged = nltk.pos_tag(tokens)
-    #print(tagged)
+    
+    sentences = nltk.tokenize.sent_tokenize(contents)
 
-    last_noun = None
-    last_verb_after_noun = None 
     NOUN_TAGS = ("NN", "NNS", "NNP")
     VERB_TAGS = ("VBZ", "VBN", "BEZ", "BEG", "VBD", "VBG","VBP")
+    fout = open("result_3.txt", 'w')
+    
+    for sentence in sentences:
+        tokens = nltk.word_tokenize(sentence)
+        tagged = nltk.pos_tag(tokens)
 
-
-    for tag in tagged:
-        if tag[1] in NOUN_TAGS:
-            if last_noun == None:
-                last_noun = tag[0]
-            else:
-                if last_verb_after_noun == None:
+        last_noun = None
+        last_verb_after_noun = None 
+        
+        for tag in tagged:
+            if tag[1] in NOUN_TAGS:
+                if last_noun == None:
                     last_noun = tag[0]
                 else:
-                    print(f"{last_noun} : {last_verb_after_noun} : {tag[0]}")
-                    last_noun = None
-                    last_verb_after_noun = None
-        elif tag[1] in VERB_TAGS:
-            last_verb_after_noun = tag[0]
+                    if last_verb_after_noun == None:
+                        last_noun = tag[0]
+                    else:
+                        fout.write(f"{sentence}\r\n")
+                        break
+            elif tag[1] in VERB_TAGS:
+                last_verb_after_noun = tag[0]
 
-
+    f.close()
         
         
-        
-
 get_nouns_with_verb_between("computer-science.txt")
 
+#Exercise 4
 
+def filter_file_content(file):
+    BASE_IRI = "http://www.co-ode.org/ontologies/ont.owl#"
+    doc = lightrdf.RDFDocument("Lab8-1.owl")
+    file_content = open("result_3.txt", 'r')
+    fwrite = open("result_4.txt", 'w')
+    for line in file_content:
+        current_line = line.rstrip()
+        found = False
+        keywords = nltk.word_tokenize(current_line)
+        for keyword in keywords:
+            for triple in doc.search_triples(f"{BASE_IRI}{keyword.lower()}", None, None):
+                found = True
+                break
+            if found:
+                print(line)
+                fwrite.write(line)
+                break
+            
+
+filter_file_content("result_4.txt")
